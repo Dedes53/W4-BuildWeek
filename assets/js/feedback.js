6a82c124b68fe0ef7754c49d23649707acff326c
Emanuele Piemonte <epiemonte1@gmail.com>
2025-12-17 11:39:35 +0100

    stars changed

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,7 +1,45 @@
-//AIAIU
-const stars = document.getElementById("stelle");
-console.log(stars);
+// AIAIU;
+// const stars = document.getElementById("star-rating");
+// console.log(stars);
 
-stars.addEventListener("click", function (event) {
-  console.log("Eccolo qua");
+// stars.addEventListener("click", function (event) {
+//   console.log("Eccolo qua");
+// });
+
+const primaStella = document.querySelector(".fas.fa-star.fa-lg.prima.stella");
+console.log(primaStella);
+
+primaStella.addEventListener("click", function (event) {
+  console.log("Ecco la prima stella");
 });
+
+// const primaStella = document.getElementsByClassName(".fas fa-star fa-lg prima stella");
+// console.log(primaStella);
+
+// primaStella.addEventListener("click", function (event) {
+//   console.log("Ecco la prima stella");
+// });
+// const secondaStella = document.querySelector(".fas fa-star fa-lg seconda stella");
+// console.log(secondaStella);
+
+// secondaStella.addEventListener("click", function (event) {
+//   console.log("Ecco la seconda stella");
+// });
+
+// ____________________________
+// ________________________________________________________
+// _______________________________________________________________________________________________________________
+// ________________________________________________________
+// ____________________________
+
+// let starRatings = document.querySelectorAll("#star-rating");
+// for (let i = 0; i < starRatings.length; i++) {
+//   (starRatings = starRatings[i]),
+//     (stellePiene = starRatings.querySelector("#stellePiene")),
+//     (rating = parseFloat(starRatings.dataset.rating) || 0),
+//     (percentWidth = rating * 80);
+
+//   stellePiene.style.percentWidth = percentWidth + "%";
+// }
+// console.log(starRatings);
+// console.log(stellePiene);
