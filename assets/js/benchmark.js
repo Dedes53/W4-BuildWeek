b3f1941bea846dfc0fe4d23e3b99f8ebe5679ddf
Dedes73 <leporfede@gmail.com>
2025-12-18 02:29:19 +0100

    change timer color

--- assets/js/benchmark.js
+++ assets/js/benchmark.js
@@ -1,10 +1,11 @@
+//implementare funzioni timer: reset alla nuova domanda e passaggio alla domanda successiva se scaduto.
 window.addEventListener("unhandledrejection", (event) => {
   event.preventDefault();
 });
 //riferimenti al dom
 const qText = document.getElementById("question"); //domanda
 const quizContainer = document.getElementById("quiz"); //risposte
-const enter = document.getElementById("confirm"); //pulsante di conferma
+const nextButton = document.getElementById("confirm"); //pulsante di conferma
 
 //domande
 const questions = [
@@ -95,12 +96,14 @@ let index = 0; //indice della domanda corrente
 let points = 0;
 
 //timer
-const time = 10;
+//timer
+// Inizializza il cerchio "già pronto"
+const time = 90;
 let bar;
 
-// === TIMER ===
 function startTimer() {
-  bar.stop(); //IMP! bisogna prima fermare l'animazione e poi richiamrla di nuovo (probailmente non cambia nulla in merito all'errore in console)
+  bar.stop(); //IMP! bisogna prima fermare l'animazione e poi richiamrla di nuovo
+  // bar.destroy?.();
   bar.set(0); //graficamente
   bar.animate(1.0); //logicamente
 }
@@ -108,7 +111,8 @@ function startTimer() {
 bar = new ProgressBar.Circle("#container", {
   color: "white",
   strokeWidth: 10,
-  trailColor: "#ada9a9ff",
+  // trailColor: "#ada9a9ff",
+  trailColor: "#070a22ff",
   trailWidth: 10,
   easing: "linear",
   duration: time * 1000,
@@ -120,28 +124,13 @@ bar = new ProgressBar.Circle("#container", {
     const value = Math.round(circle.value() * time);
     circle.setText(time - value);
     circle.text.style.fontSize = "40px";
-    if (value === time) {
-      nextQuestion();
-    }
   },
 });
+//
 // speriamo funzioni
 
-// === GESTIONE DEL QUIZ ===
-
-// funzione per passare alla domanda successiva
-function nextQuestion() {
-  index++;
-  if (index < questions.length) {
-    showQuestion();
-    startTimer();
-  } else {
-    window.location.href = "results.html";
-    console.log("Punteggio finale:", points);
-    sendPoints(points);
-  }
-}
-
+//
+//
 // Mescola casualmente gli elementi di un array
 function shuffleArray(array) {
   return array.sort(() => Math.random() - 0.5);
@@ -158,7 +147,8 @@ function createAnswer(text, isCorrect, index, answers) {
   radio.name = "question-" + index; //forse superfluo
   radio.value = text;
 
-  // aggiungiamo un data-attribute se è la risposta corretta
+  // se è la risposta corretta,
+  // aggiungiamo un data-attribute
   if (isCorrect) {
     radio.dataset.correct = "true";
   }
@@ -193,27 +183,55 @@ function showQuestion() {
   shuffleArray(answers).forEach((answer) => quizContainer.appendChild(answer));
 }
 
-enter.addEventListener("click", () => {
+quizContainer.after(nextButton); // inserisce il bottone dopo il quiz
+
+nextButton.addEventListener("click", () => {
   const selected = document.querySelector(`input[name="question-${index}"]:checked`); // radio button selezionato
 
   if (!selected) {
-    nextQuestion();
+    index++;
+    startTimer();
+    if (index < questions.length) {
+      showQuestion();
+    } else {
+      window.location.href = "results.html";
+      sendPoints(points);
+    }
     return;
   }
 
   // se la risposta è corretta
   if (selected.dataset.correct === "true") {
     points++;
+    startTimer(); //riavvia
   }
+  // if(timer finito){
+  // passa ad un altra domanda}
 
-  nextQuestion();
+  index++; // incremento l'indice per passare alla domanda successiva
+
+  // se ci sono ancora domande
+  if (index < questions.length) {
+    showQuestion();
+    startTimer(); //riavvia
+  } else {
+    // FINE QUIZ
+    // qText.textContent = "Quiz terminato!";
+    // quizContainer.innerHTML = "";
+    // nextButton.disabled = true;
+    window.location.href = "results.html";
+
+    // punteggio visibile solo in console
+    console.log("Punteggio finale:", points);
+    sendPoints(points);
+  }
 });
 
 function sendPoints(points) {
   localStorage.setItem("points", points);
 }
 
-//=== AVVIO DEL QUIZ ===
+//avvia il quiz
 window.onload = function () {
   showQuestion();
   startTimer();
